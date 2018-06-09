005ceae54b1925e42229feaab72af889d794e0be
Legend <jasonrfelipe@gmail.com>
2018-06-09 15:59:21 -0700

    added search functionality to custom buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,64 +1,80 @@
 
-var gifButtons = ["Mario", "Sonic", "Kirby"];
+var gifButtons = ["Mario", "Sonic", "Kirby", "Solid Snake", "Ryu", "Master Chief",];
+
+
+
 
 for (i = 0; i < gifButtons.length; i++){
-    $('#gameButtons').append("<button id='gameButton' style='margin:5px' data-game=" + gifButtons[i] + ">" + gifButtons[i] + "</button>"); //for some reason giving #buttons .attr didn't work, so I brute forced it.
+    var gameButtons = $('<button>'+ gifButtons[i] + '</button>');
+    gameButtons.attr("class", "showGif");
+    gameButtons.attr("style", "margin:5px");
+    gameButtons.attr("data-game", gifButtons[i]);
+    $('#gameButtons').append(gameButtons)
+      }
+
+
+  $('#search').click(function(e){
+
+    e.preventDefault();
     
-  }
-
-console.log(gameButtons)
-
-$('#gameButton').on("click", function() {
-    var game = $(this).attr("data-game");
+    var searchTerm = $('#search-term').val().trim();
     
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      game + "&api_key=S2WxAN0l1NhiYgOVUn8rPyjLMMlcPbXK&limit=10&tag=game";
+    gifButtons.push(searchTerm);
     
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-    .then(function(response) {
-      var results = response.data;
+   var searchedGame = $('<button>' + searchTerm + '</button>');
+   searchedGame.attr({ // entering ALL  attributes
+        "class": "showGif",
+        "style": "margin:5px",
+        "data-game": searchTerm 
+   });
 
-      for (var i = 0; i < results.length; i++) {
+   $('#gameButtons').append(searchedGame);
+   $('.showGif').on("click", theSearch);
 
-        if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-          var gifDiv = $("<div class='item'>");
-
-          var rating = results[i].rating;
-
-          var p = $("<p>").text("Rating: " + rating);
-
-          var gameImage = $("<img>");
-
-          gameImage.attr("src", results[i].images.fixed_height.url);
-
-          gifDiv.append(p);
-          gifDiv.append(gameImage);
-
-          $("#gifs-appear-here").prepend(gifDiv);
-
-        }
-    }
+   
   });
-});
 
-$('#search').click(function(e){
 
-e.preventDefault();
-
-var searchTerm = $('#search-term').val().trim();
-
-console.log(searchTerm)
-//add script to put button.
-gifButtons.push(searchTerm);
-console.log(gifButtons)
-
-$('#gameButtons').append("<button id='gameButton' style='margin:5px' data-game=>" + searchTerm + "</button>")
+//The click event
+$('.showGif').on("click", theSearch);
 
 
 
+//The separate function.
+function theSearch() {
+  var game = $(this).attr("data-game");
 
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    game + "&api_key=S2WxAN0l1NhiYgOVUn8rPyjLMMlcPbXK&limit=10";
+
+
+  
+$.ajax({
+  url: queryURL,
+  method: "GET"
 })
-    
+  .then(function(response) {
+    var results = response.data;
+
+    for (var i = 0; i < results.length; i++) {
+
+      if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+        var gifDiv = $("<div class='item'>");
+
+        var rating = results[i].rating;
+
+        var p = $("<p>").text("Rating: " + rating);
+
+        var gameImage = $("<img>");
+
+        gameImage.attr("src", results[i].images.fixed_height.url);
+
+        gifDiv.append(p);
+        gifDiv.append(gameImage);
+
+        $("#gifs-appear-here").prepend(gifDiv);
+
+      }
+  }
+});
+}
