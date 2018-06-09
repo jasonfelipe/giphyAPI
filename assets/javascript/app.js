31d1c429aabb5c417fd5e1244293711a40e8b776
Legend <jasonrfelipe@gmail.com>
2018-06-09 00:10:11 -0700

    updated all files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,51 +1,41 @@
 
-//add script to make for loop that creates buttons.
+var gifButtons = ["Mario", "Sonic", "Kirby"];
+
+for (i = 0; i < gifButtons.length; i++){
+    $('#gameButtons').append("<button style='margin:5px' data-game=" + gifButtons[i] + ">" + gifButtons[i] + "</button>"); //for some reason giving #buttons .attr didn't work, so I brute forced it.
+}
+console.log(gameButtons)
 
 
-
-
-$("button").on("click", function() {
-    // In this case, the "this" keyword refers to the button that was clicked
+$('button').on("click", function() {
     var game = $(this).attr("data-game");
 
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      game + "&api_key=S2WxAN0l1NhiYgOVUn8rPyjLMMlcPbXK&limit=10";
-
+      game + "&api_key=S2WxAN0l1NhiYgOVUn8rPyjLMMlcPbXK&limit=10&tag=game";
+    console.log(queryURL)
 $.ajax({
     url: queryURL,
     method: "GET"
   })
-    // After the data comes back from the API
     .then(function(response) {
-      // Storing an array of results in the results variable
       var results = response.data;
 
-      // Looping over every result item
       for (var i = 0; i < results.length; i++) {
 
-        // Only taking action if the photo has an appropriate rating
         if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-          // Creating a div with the class "item"
           var gifDiv = $("<div class='item'>");
 
-          // Storing the result item's rating
           var rating = results[i].rating;
 
-          // Creating a paragraph tag with the result item's rating
           var p = $("<p>").text("Rating: " + rating);
 
-          // Creating an image tag
           var gameImage = $("<img>");
 
-          // Giving the image tag an src attribute of a proprty pulled off the
-          // result item
           gameImage.attr("src", results[i].images.fixed_height.url);
 
-          // Appending the paragraph and personImage we created to the "gifDiv" div we created
           gifDiv.append(p);
           gifDiv.append(gameImage);
 
-          // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
           $("#gifs-appear-here").prepend(gifDiv);
 
         }
@@ -55,15 +45,13 @@ $.ajax({
 
 $('#submit').click(function(e){
 
-    e.preventDefault();
+e.preventDefault();
 
 var searchTerm = $('#search-term').val().trim();
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-searchTerm + "&api_key=S2WxAN0l1NhiYgOVUn8rPyjLMMlcPbXK&limit=10";
 
 //add script to put button.
-
+gifButtons.push(searchTerm);
 
 
     })
